b45ecb30755bf4baa1f39466aedd8a757ad09ccb
Benjamin Small <ben20262@aim.com>
2020-01-26 00:11:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ class Student
       WHERE name = ?
     SQL
     array = DB[:conn].execute(sql, name)
-    student = Student.new_from_db (array)
+    student = self.class.new_from_db (array)
     student
   end
 
