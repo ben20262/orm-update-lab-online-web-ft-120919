15d6b8c59d74e3a2182562ba6918021baafa1a4f
Benjamin Small <ben20262@aim.com>
2020-01-26 00:01:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,11 +32,15 @@ class Student
   end
 
   def save
-    sql = <<-SQL
-    INSERT INTO students (name, grade) VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, @name, @grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    if @id
+      update
+    else
+      sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, @name, @grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
 
   def self.create (name, grade)
